8b3d28cd0f28afdc7f45eb02818504ce177f42bb
patrickvossler18 <patrick.vossler18@gmail.com>
2016-11-21 23:11:19 -0800

    Change main routine to take arguments

--- main.py
+++ main.py
@@ -8,22 +8,18 @@ import foreign_court_citations
 from tqdm import tqdm
 
 def main(args=None):
-    COUNTRY_LIST = ['Austria','Australia','Botswana','Canada','Chile','Colombia','France','Germany' ,'Ireland']
     #create regex tables just once
-    soft_law_regex_table,soft_law_regex_df = regex_tables.createSoftLawRegexDf(folder_path="/Users/patrick/Dropbox/Fall 2016/SPEC/Regex tables/",file_name='softlaw_regex_20161003.csv')
-    intl_court_regex_table,intl_court_regex_df = regex_tables.createIntlCourtsRegexDf(folder_path="/Users/patrick/Dropbox/Fall 2016/SPEC/Regex tables/",file_name= 'intl_courts_regex_20161003.csv')
-    treaties_regex_table,treaties_regex_df = regex_tables.createTreatiesRegexDf(folder_path="/Users/patrick/Dropbox/Fall 2016/SPEC/Regex tables/",file_name= 'treaties_regex_20161003.csv')
-    fc_regex_table,fc_regex_df = regex_tables.createForeignCourtsDf(folder_path="/Users/patrick/Dropbox/Fall 2016/SPEC/Regex tables/",file_name='foreign_courts_regex_20161007.csv')
+    soft_law_regex_table,soft_law_regex_df = regex_tables.createSoftLawRegexDf(folder_path=REGEX_FOLDER,file_name='softlaw_regex_20161003.csv')
+    intl_court_regex_table,intl_court_regex_df = regex_tables.createIntlCourtsRegexDf(folder_path=REGEX_FOLDER,file_name= 'intl_courts_regex_20161003.csv')
+    treaties_regex_table,treaties_regex_df = regex_tables.createTreatiesRegexDf(folder_path=REGEX_FOLDER,file_name= 'treaties_regex_20161003.csv')
+    fc_regex_table,fc_regex_df = regex_tables.createForeignCourtsDf(folder_path=REGEX_FOLDER,file_name='foreign_courts_regex_20161007.csv')
 
     #connect to mysql server
-    TABLE_NAME = 'citations'
-    DATABASE_NAME ='cglp'
-    PASSWORD = 'cglp'
     ENGINE = helpers.connectDb(DATABASE_NAME,PASSWORD)
 
     for country in COUNTRY_LIST:
         print country
-        countryFiles = helpers.getCountryFiles("/Users/patrick/Dropbox/Fall 2016/SPEC/CGLP Data",country)
+        countryFiles = helpers.getCountryFiles(DATA_FOLDER,country)
         for year, folder in countryFiles.items():
             #go through dictionary of files and insert into mysql table
             for file in tqdm(folder):
@@ -33,4 +29,28 @@ def main(args=None):
                 foreign_court_citations.insertForeignCourtsData(country,year,file,fc_regex_df,fc_regex_table,TABLE_NAME,ENGINE)
 
 if __name__ == "__main__":
+    try:
+        options,arguments = getopt.getopt(sys.argv[1:], "r:d:t:p:c:n:",["regex_folder=","database_name=","table_name=","mysql_password=","cglp_data_folder=","country="])
+    except getopt.GetoptError as error:
+        sys.exit("Getopt Error: " + str(error))
+
+    # set up default values
+    COUNTRY_LIST = ['Austria','Australia','Botswana','Canada','Chile','Colombia','France','Germany' ,'Ireland']
+    TABLE_NAME = 'citations'
+    DATABASE_NAME ='cglp'
+    PASSWORD = 'cglp'
+
+    for o,a in options:
+        if o in ["-r","--regex_folder"]:
+            REGEX_FOLDER = a
+        elif o in ["-d","--database_name"]:
+            DATABASE_NAME = a 
+        elif o in ["-t","--table_name"]:
+            TABLE_NAME = a
+        elif o in ["-p","--mysql_password"]:
+            PASSWORD = a
+        elif o in ["-c","--cglp_data_folder"]:
+            DATA_FOLDER = a
+        elif o in ["-n","--country_name"]:
+            country = a
     main()
