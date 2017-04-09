73c030b714ac4f391145799a5c6853d3127e1d92
patrickvossler18 <patrick.vossler18@gmail.com>
2017-04-09 15:47:31 -0700

    Extend list error fixed

--- extract_court_refs.py
+++ extract_court_refs.py
@@ -1139,7 +1139,7 @@ def insertCaseRefData(case_info, country_name, country_df, year, id,
     try:
         case_info_list = list(case_info)
         case_info_list.extend([country_df.loc[country_name][0], year, id])
-        case_info_list.extend(source_file)
+        case_info_list.extend([source_file])
         case_info_df = pd.DataFrame(columns=['case_id', 'decision_date',
                                              'participant_name', 'country_id',
                                              'year', 'id', 'source_file_name'])
