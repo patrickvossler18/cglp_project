36bcc9a30ce404bae83de271932a72a34f26ea79
patrickvossler18 <patrick.vossler18@gmail.com>
2017-07-18 20:09:56 -0700

    Add spain, use uuid

--- main.py
+++ main.py
@@ -11,126 +11,77 @@ import getopt
 # import logging
 from time import gmtime, strftime
 import csv
-import concurrent.futures
+import multiprocessing as mp
+import pandas as pd
 
 
-def get_references(REGEX_FOLDER, DATA_FOLDER):
-    # Initialize logger
-    # start_time = strftime("%d-%m-%Y", gmtime())
-    # logging.basicConfig(filename='/tmp/get_references_%s.log' % start_time, level=logging.DEBUG,
-    #                     format='[%(asctime)s %(filename)s:%(lineno)s - %(funcName)20s()] %(message)s')
-    #                     # format='%(asctime)s %(levelname)s %(name)s %(message)s')
-    # logger = logging.getLogger(__name__)
-
-    # create regex tables just once
-    softlaw_names, soft_law_regex_df = rt.createSoftLawRegexDf(folder_path=REGEX_FOLDER, file_name='softlaw_regex_20161003.csv')
-    intl_court_names, intl_court_regex_df = rt.createIntlCourtsRegexDf(folder_path=REGEX_FOLDER, file_name='intl_courts_regex_20161003.csv')
-    treaty_names, treaties_regex_df = rt.createTreatiesRegexDf(folder_path=REGEX_FOLDER, file_name='treaties_regex_20161003.csv')
-    fc_country_names, fc_court_names, fc_regex_df = rt.createForeignCourtsDf(folder_path=REGEX_FOLDER, file_name='foreign_courts_regex_20161007.csv')
-    country_df = rt.createCountryDf(folder_path=REGEX_FOLDER, file_name='country_ids_20161210.csv')
-
-    # connect to mysql server and create tables
-    helpers.createTables(DATABASE_NAME, PASSWORD, drop_table=True)
-    ENGINE = helpers.connectDb(DATABASE_NAME, PASSWORD)
-    global ID_VAR
-    ID_VAR = 1
-    error_log = []
-    written_log = False
-
-    def insertData(file):
-        if country == 'United Kingdom':
-                        for function in cr.countryRefFunctions[country]:
-                            case = function(file)
-                            if case is not None:
-                                caseInfo = case
-                                # break
-        else:
-            caseInfo = cr.countryRefFunctions[country](file)
-        if caseInfo is not None:
-            cr.insertCaseRefData(case_info=caseInfo,
-                                 country_name=country,
-                                 country_df=country_df,
-                                 year=year,
-                                 id=ID_VAR,
-                                 source_file=file,
-                                 mysql_table=CASE_TABLE_NAME,
-                                 connection_info=ENGINE)
-        # Fix for pdfs because esm only accepting strings, not unicode
-        fileText = helpers.getFileText(file, html=True, pdf_utf8=False)
-        if fileText is not None:
-            sl.insertSoftLawData(country_name=country, year=year,
-                                 file=file, fileText=fileText,
-                                 regex_df=soft_law_regex_df,
-                                 softlaw_names=softlaw_names,
-                                 id_num=ID_VAR,
-                                 mysql_table=CITATION_TABLE_NAME,
-                                 connection_info=ENGINE,
-                                 country_df=country_df)
-            ic.insertIntlCourtData(country_name=country,
-                                   year=year,
-                                   file=file,
-                                   fileText=fileText,
-                                   regex_df=intl_court_regex_df,
-                                   intl_court_names=intl_court_names,
-                                   id_num=ID_VAR,
-                                   mysql_table=CITATION_TABLE_NAME,
-                                   connection_info=ENGINE,
-                                   country_df=country_df)
-            tc.insertTreatyData(country_name=country,
-                                year=year,
-                                file=file,
-                                fileText=fileText,
-                                regex_df=treaties_regex_df,
-                                treaty_names=treaty_names,
-                                id_num=ID_VAR,
-                                mysql_table=CITATION_TABLE_NAME,
-                                connection_info=ENGINE,
-                                country_df=country_df)
-            fc.insertForeignCourtsData(country_name=country,
-                                       year=year,
-                                       file=file,
-                                       fileText=fileText,
-                                       regex_df=fc_regex_df,
-                                       country_names=fc_country_names,
-                                       court_names=fc_court_names,
+def insertData(file_info):
+    file = file_info[0]
+    ID_VAR = file_info[1]
+    year = file_info[2]
+    if country == 'United Kingdom':
+                    for function in cr.countryRefFunctions[country]:
+                        case = function(file)
+                        if case is not None:
+                            caseInfo = case
+                            # break
+    else:
+        caseInfo = cr.countryRefFunctions[country](file)
+    if caseInfo is not None:
+        case_res = cr.insertCaseRefData(case_info=caseInfo,
+                                        country_name=country,
+                                        country_df=country_df,
+                                        year=year,
+                                        id=ID_VAR,
+                                        source_file=file,
+                                        mysql_table=CASE_TABLE_NAME,
+                                        connection_info=ENGINE)
+    # Fix for pdfs because esm only accepting strings, not unicode
+    fileText = helpers.getFileText(file, html=True, pdf_utf8=False)
+    if fileText is not None:
+        softLaw = sl.insertSoftLawData(country_name=country, year=year,
+                                       file=file, fileText=fileText,
+                                       regex_df=soft_law_regex_df,
+                                       softlaw_names=softlaw_names,
                                        id_num=ID_VAR,
                                        mysql_table=CITATION_TABLE_NAME,
                                        connection_info=ENGINE,
                                        country_df=country_df)
-        global ID_VAR
-        ID_VAR += 1
+        intlCourt = ic.insertIntlCourtData(country_name=country,
+                                           year=year,
+                                           file=file,
+                                           fileText=fileText,
+                                           regex_df=intl_court_regex_df,
+                                           intl_court_names=intl_court_names,
+                                           id_num=ID_VAR,
+                                           mysql_table=CITATION_TABLE_NAME,
+                                           connection_info=ENGINE,
+                                           country_df=country_df)
+        treaty = tc.insertTreatyData(country_name=country,
+                                    year=year,
+                                    file=file,
+                                    fileText=fileText,
+                                    regex_df=treaties_regex_df,
+                                    treaty_names=treaty_names,
+                                    id_num=ID_VAR,
+                                    mysql_table=CITATION_TABLE_NAME,
+                                    connection_info=ENGINE,
+                                    country_df=country_df)
+        foreignCourt = fc.insertForeignCourtsData(country_name=country,
+                                                  year=year,
+                                                  file=file,
+                                                  fileText=fileText,
+                                                  regex_df=fc_regex_df,
+                                                  country_names=fc_country_names,
+                                                  court_names=fc_court_names,
+                                                  id_num=ID_VAR,
+                                                  mysql_table=CITATION_TABLE_NAME,
+                                                  connection_info=ENGINE,
+                                                  country_df=country_df)
+        return case_res, softLaw, intlCourt, treaty, foreignCourt
 
-    for country in COUNTRY_LIST:
-        print country
-        countryFiles = helpers.getCountryFiles(DATA_FOLDER, country)
-        if country == 'USA':
-            country = 'United States'
-        if country == 'UK':
-            country = 'United Kingdom'
 
-        with concurrent.futures.ProcessPoolExecutor() as executor:
-            for year, folder in countryFiles.items():
-                # go through dictionary of files and insert into mysql table
-                try:
-                    executor.map(insertData, folder)
-                except Exception, e:
-                    print e
-                    ID_VAR += 1
-                    pass
-
-
-if __name__ == "__main__":
-    try:
-        options, arguments = getopt.getopt(sys.argv[1:], "r:d:t:a:p:c:n:",
-                                           ["regex_folder=", "database_name=",
-                                            "citation_table_name=",
-                                            "case_table_name",
-                                            "mysql_password=",
-                                            "cglp_data_folder=", "country="])
-    except getopt.GetoptError as error:
-        sys.exit("Getopt Error: " + str(error))
-
-    # set up default values
+if __name__ == '__main__':
     COUNTRY_LIST = ['Australia', 'Austria', 'Botswana', 'Belgium', 'Canada',
                     'Chile', 'Colombia', 'France', 'Germany', 'India',
                     'Ireland', 'Lesotho', 'Malawi', 'Malaysia', 'New Zealand',
@@ -142,23 +93,58 @@ if __name__ == "__main__":
     CASE_TABLE_NAME = 'case_info_test'
     DATABASE_NAME = 'cglp'
     PASSWORD = 'cglp'
-
-    for o, a in options:
-        if o in ["-r", "--regex_folder"]:
-            REGEX_FOLDER = a
-        elif o in ["-d", "--database_name"]:
-            DATABASE_NAME = a
-        elif o in ["-t", "--citation_table_name"]:
-            CITATION_TABLE_NAME = a
-        elif o in ["-a", "--case_table_name"]:
-            CASE_TABLE_NAME = a
-        elif o in ["-p", "--mysql_password"]:
-            PASSWORD = a
-        elif o in ["-c", "--cglp_data_folder"]:
-            DATA_FOLDER = a
-        elif o in ["-n", "--country_name"]:
-            COUNTRY_LIST = [a]
+    REGEX_FOLDER = '/home/ec2-user/regex_tables/'
+    DATA_FOLDER = '/home/ec2-user/CGLP_Data'
+    softlaw_names, soft_law_regex_df = rt.createSoftLawRegexDf(folder_path=REGEX_FOLDER, file_name='softlaw_regex_20161003.csv')
+    intl_court_names, intl_court_regex_df = rt.createIntlCourtsRegexDf(folder_path=REGEX_FOLDER, file_name='intl_courts_regex_20161003.csv')
+    treaty_names, treaties_regex_df = rt.createTreatiesRegexDf(folder_path=REGEX_FOLDER, file_name='treaties_regex_20161003.csv')
+    fc_country_names, fc_court_names, fc_regex_df = rt.createForeignCourtsDf(folder_path=REGEX_FOLDER, file_name='foreign_courts_regex_20161007.csv')
+    country_df = rt.createCountryDf(folder_path=REGEX_FOLDER, file_name='country_ids_20161210.csv')
+    # connect to mysql server and create tables
+    helpers.createTables(DATABASE_NAME, PASSWORD, drop_table=True)
+    ENGINE = helpers.connectDb(DATABASE_NAME, PASSWORD)
+    BATCH_START = 1
+    error_log = []
+    pool = mp.Pool()
     run_start_time = strftime("%d-%m-%Y %H:%M:%S", gmtime())
-    get_references(REGEX_FOLDER, DATA_FOLDER)
+    for country in [COUNTRY_LIST[0]]:
+        print country
+        countryFiles = helpers.getCountryFiles(DATA_FOLDER, country)
+        if country == 'USA':
+            country = 'United States'
+        if country == 'UK':
+            country = 'United Kingdom'
+        for year, folder in countryFiles.items():
+            try:
+                print year
+                # data = list(zip(folder, list(range(BATCH_START, BATCH_START + len(folder)+1)), [year] * len(folder)))
+                results = pool.map(insertData, list(zip(folder, [str(uuid.uuid4()) for i in range(len(folder))], [year] * len(folder))))
+                BATCH_START += len(folder)
+                for result in results:
+                    if result is not None:
+                        if result[0] is not None:
+                            result[0].to_sql(name=CASE_TABLE_NAME, con=ENGINE,
+                                             index=False, if_exists='append')
+                        if result[1] is not None:
+                            result[1].to_sql(name=CITATION_TABLE_NAME, con=ENGINE,
+                                             index=False, if_exists='append')
+                        if result[2] is not None:
+                            result[2].to_sql(name=CITATION_TABLE_NAME, con=ENGINE,
+                                             index=False, if_exists='append')
+                        if result[3] is not None:
+                            result[3].to_sql(name=CITATION_TABLE_NAME, con=ENGINE,
+                                             index=False, if_exists='append')
+                        if result[4] is not None:
+                            result[4].to_sql(name=CITATION_TABLE_NAME, con=ENGINE,
+                                             index=False, if_exists='append')
+            except Exception, e:
+                print e
+                error_log.append('%s, %s, %s' % (country, file, e))
+                pass
+    if len(error_log) > 0:
+        start_time = strftime("%d-%m-%Y", gmtime())
+        with open('/home/ec2-user/error_log_%s.csv' % (start_time), 'wb') as csvfile:
+            wr = csv.writer(csvfile)
+            wr.writerow(error_log)
     run_end_time = strftime("%d-%m-%Y %H:%M:%S", gmtime())
     print run_start_time, run_end_time
